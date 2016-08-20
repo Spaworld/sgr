9b8359e8c795a9010ecdf2aa35ffe52673a8e74b
dimanyc <dimitrynazarov@gmail.com>
2016-08-20 08:58:40 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+['price', 'quality', 'warranty', 'installation', 'vapor', 'heat', 'noise', 'maintenance'].each do |feature_name|
+  feature           = FactoryGirl.create(:feature, :with_rating,  name: feature_name.capitalize)
+  feature_photo     = FactoryGirl.create(:photo, file:File.new("#{Rails.root}/spec/support/images/#{feature_name}.png"))
+  feature.photos           << feature_photo
+end
+
+['Amerec', 'Kohler', 'MrSteam', 'Steamist', 'SteamSpa', 'Thermasol'].each do |brand_name|
+  brand             = FactoryGirl.create(:brand, name: brand_name)
+  brand_photo       = FactoryGirl.create(:photo, file: File.new("#{Rails.root}/spec/support/images/#{brand_name.downcase}-logo.jpg") )
+  product           = FactoryGirl.create(:product, title: "#{brand_name} Steam Generator")
+  product_photo     = FactoryGirl.create(:photo, file: File.new("#{Rails.root}/spec/support/images/#{brand_name.downcase}-steam-generator.jpg") )
+  brand.photos      << brand_photo
+  product.photos    << product_photo
+  brand.products    << product
+  product.features  << Feature.all
+end
+
